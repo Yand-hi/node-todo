46ce47aebe4d33f7b76d7a4510feff833dc8ec03
Yandhi <15877410869@163.com>
2022-01-15 03:54:27 +0800

    优化代码，封装函数

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const db = require('./db')
 const inquirer = require('inquirer')
 
 module.exports.add = async (title) => {
-  const task = {
-    title: title,
-    done: false
-  }
   // 读取之前的任务
   const list = await db.read()
   // 添加一个新任务
-  list.push(task)
+  list.push({title: title, done: false})
   // 存储任务到文件
   await db.write(list)
 }
@@ -22,6 +18,10 @@ module.exports.show = async () => {
   // 读取之前的 list
   const list = await db.read()
   // 切换进行操作任务
+  printTasks(list)
+}
+
+function printTasks(list) {
   inquirer
     .prompt({
       type: 'list',
@@ -35,63 +35,69 @@ module.exports.show = async () => {
     }).then((answer) => {
     const index = parseInt(answer.index)
     if (index >= 0) {
-      // 改变任务状态
-      inquirer
-        .prompt({
-          type: 'list',
-          name: 'action',
-          message: '请选择操作',
-          choices: [
-            {name: '标记为完成', value: 'markAsDone'},
-            {name: '标记为未完成', value: 'markAsUnDone'},
-            {name: '修改标题', value: 'updateTitle'},
-            {name: '删除', value: 'remove'},
-            {name: '退出', value: 'quit'},
-          ]
-        }).then((answer2) => {
-        switch (answer2.action) {
-          case 'markAsDone':
-            list[index].done = true
-            db.write(list)
-            console.log('已标记为完成')
-            break
-          case 'markAsUnDone':
-            list[index].done = false
-            db.write(list)
-            console.log('已标记为未完成')
-            break
-          case 'remove':
-            list.splice(index, 1)
-            db.write(list)
-            console.log('已删除')
-            break
-          case 'updateTitle':
-            inquirer
-              .prompt({
-                type: 'input',
-                name: 'title',
-                message: '请输入标签名',
-                default: list[index].title
-              }).then((answer3) => {
-              list[index].title = answer3.title
-              db.write(list)
-              console.log('修改成功')
-            })
-            break
-        }
-      })
+      askForAction(list, index)
     } else if (index === -1) {
-      // 添加任务
-      inquirer
-        .prompt({
-          type: 'creat',
-          name: 'title',
-          message: '请输入任务名称',
-        }).then((answer4) => {
-        list.push({title: answer4.title, done: false})
-        db.write(list)
-        console.log('添加成功')
-      })
+      askForCreatTask(list)
     }
+  })
+}
+
+function askForAction(list, index) {
+  const actions = {markAsDone, markAsUnDone, remove, updateTitle}
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: '请选择操作',
+      choices: [
+        {name: '标记为完成', value: 'markAsDone'},
+        {name: '标记为未完成', value: 'markAsUnDone'},
+        {name: '修改标题', value: 'updateTitle'},
+        {name: '删除', value: 'remove'},
+        {name: '退出', value: 'quit'},
+      ]
+    }).then((answer) => {
+    const action = actions[answer.action]
+    action && action(list, index)
+  })
+}
+
+function markAsDone(list, index) {
+  list[index].done = true
+  db.write(list).then(() => {console.log('已标记为完成')})
+}
+
+function markAsUnDone(list, index) {
+  list[index].done = false
+  db.write(list).then(() => {console.log('已标记为未完成')})
+}
+
+function remove(list, index) {
+  list.splice(index, 1)
+  db.write(list).then(() => { console.log('已删除')})
+}
+
+function updateTitle(list, index) {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'title',
+      message: '请输入标签名',
+      default: list[index].title
+    }).then((answer) => {
+    list[index].title = answer.title
+    db.write(list).then(() => {console.log('修改成功')})
+  })
+}
+
+function askForCreatTask(list) {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'title',
+      message: '请输入任务名称',
+    }).then((answer) => {
+    list.push({title: answer.title, done: false})
+    db.write(list).then(() => {console.log('添加成功')})
   })
 }
