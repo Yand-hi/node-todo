7b3a7370897894f2744c454da542b22880fcbff1
Yandhi <15877410869@163.com>
2022-01-14 23:51:01 +0800

    node cli => show all task

--- cli.js
+++ cli.js
@@ -9,12 +9,18 @@ program
   .action((...args) => {
     const words = args.slice(0, -1).join(' ')
     api.add(words)
+      .then(() => {console.log('添加成功')}, () => {console.log('添加失败')})
   });
 program
   .command('clear')
   .description('clear all task')
   .action(() => {
     api.clear()
+      .then(() => {console.log('清除成功')}, () => {console.log('清楚失败')})
   });
+
+if (process.argv.length === 2) {
+  void api.show()
+}
 
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -16,3 +16,11 @@ module.exports.add = async (title) => {
 module.exports.clear = async () => {
   await db.write([])
 }
+
+module.exports.show = async () => {
+  // 读取之前的 list
+  const list = await db.read()
+  list.forEach((item, index) => {
+    console.log(`${item.done ? '[√]' : '[x]'} ${index + 1}：${item.title}`)
+  })
+}
