21737c322ac414c440b0e9e5a2f445ddc4db031c
Yandhi <15877410869@163.com>
2022-01-14 23:15:17 +0800

    封装接口

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-const homedir = require('os').homedir()
-const home = process.env.HOME || homedir
-const p = require('path')
-const fs = require('fs')
-const dbPath = p.join(home, '.todo')
-
-module.exports.add = (title) => {
+const db = require('./db')
+module.exports.add = async (title) => {
+  const task = {
+    title: title,
+    done: false
+  }
   // 读取之前的任务
-  fs.readFile(dbPath, {flag: 'a+'}, (error, data) => {
-    if (error) {console.log(error)} else {
-      let list
-      try {
-        list = JSON.parse(data.toString())
-      } catch (error2) {
-        list = []
-      }
-      console.log(list)
-      const task = {
-        title: title,
-        done: false
-      }
-      list.push(task)
-      const string = JSON.stringify(list)
-      fs.writeFile(dbPath, string+'\n', (error3) => {
-        if (error3) {console.log(error3)}
-      })
-    }
-  })
+  const list = await db.read()
   // 添加一个新任务
+  list.push(task)
   // 存储任务到文件
+  await db.write(list)
 }
